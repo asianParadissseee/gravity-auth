f7d11dbae7c36d89f8444e68799ab836ec246c24
asianParadissseee <asian.paradissseee@gmail.com>
2025-06-18 21:21:07 +0500

    added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/image', '@nuxt/icon', '@nuxt/fonts']
+  modules: ['@nuxt/ui', '@nuxt/image', '@nuxt/icon', '@nuxt/fonts'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  }
 })
