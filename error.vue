766d53bbdfd6a6e93dc8a198cd0faf1cb9aba717
asianParadissseee <asian.paradissseee@gmail.com>
2025-06-18 21:26:33 +0500

    added page and pinia

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Sorry bro error, please try again later.
+    <
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/image', '@nuxt/icon', '@nuxt/fonts'],
+  modules: ['@nuxt/ui', '@nuxt/image', '@nuxt/icon', '@nuxt/fonts', '@pinia/nuxt'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
